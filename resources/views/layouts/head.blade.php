eba33b779a89deb5e647b731738f46911fc83a48
emanuelnpw <emanuelnpw@gmail.com>
2026-06-05 23:00:26 +0700

    feat: initialize vite + react

--- resources/views/layouts/head.blade.php
+++
@@ -1,7 +0,0 @@
-    <link rel="shortcut icon" href="{{ URL::asset('assets/images/favicon.ico')}}">
-
-    @yield('css')
-
-    <link href="{{ URL::asset('assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
-    <link href="{{ URL::asset('assets/css/icons.css')}}" rel="stylesheet" type="text/css">
-    <link href="{{ URL::asset('assets/css/style.css')}}" rel="stylesheet" type="text/css">
